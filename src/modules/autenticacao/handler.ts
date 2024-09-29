451e4e50ce50b2903226df62c44991925366e89d
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-09-28 23:20:50 -0300

    feat: adds zod error handler

--- src/modules/autenticacao/handler.ts
+++ src/modules/autenticacao/handler.ts
@@ -1,5 +1,7 @@
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { autenticarFactory as autenticarControllerFactory } from './factory';
+import { ZodError } from 'zod';
+import { fromZodError } from 'zod-validation-error';
 
 /*
 	Run this function locally with the following command:
@@ -9,9 +11,28 @@ class Handler {
 	static async handle(
 		event: APIGatewayProxyEvent,
 	): Promise<APIGatewayProxyResult> {
-		const body = JSON.parse(event.body || '{}');
-		const controller = autenticarControllerFactory();
-		return controller.run(body);
+		try {
+			const body = JSON.parse(event.body || '{}');
+			const controller = autenticarControllerFactory();
+			return controller.run(body);
+		} catch (error) {
+			if (error instanceof ZodError) {
+				const zodError = fromZodError(error);
+				return {
+					statusCode: 400,
+					body: JSON.stringify({
+						message: 'Erro de validação',
+						details: JSON.stringify({ ...zodError, stack: zodError.stack })
+					}),
+				};
+			}
+			return {
+				statusCode: 500,
+				body: JSON.stringify({
+					message: 'Erro interno'
+				}),
+			};
+		}
 	}
 }
 
