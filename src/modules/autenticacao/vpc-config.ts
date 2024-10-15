e88d8633ed0e7968b748c861070da106b7ac99cc
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-15 18:20:51 -0300

    fix: gets envs from dotenv

--- src/modules/autenticacao/vpc-config.ts
+++ src/modules/autenticacao/vpc-config.ts
@@ -1,2 +1,6 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 export const securityGroupIds = [process.env.SECURITY_GROUP_ID!]
 export const subnetIds = [process.env.SUBNET_ID_1A!, process.env.SUBNET_ID_1B!]
