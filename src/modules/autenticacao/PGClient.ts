29ab1af5dbbf949322ef9f1d09e3fbb7ea164847
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-15 18:38:35 -0300

    refactor: refactors db conn

--- src/modules/autenticacao/PGClient.ts
+++ src/modules/autenticacao/PGClient.ts
@@ -6,7 +6,12 @@ export class PGClient implements DBClient {
 
     constructor() {
         this.pool = new Pool({
-            connectionString: process.env.DATABASE_URL
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            port: parseInt(process.env.DB_PORT!),
+            host: process.env.DB_HOST,
+            ssl: true
         });
     }
 
