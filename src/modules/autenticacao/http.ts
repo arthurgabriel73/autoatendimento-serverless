d7a040020a21c5f18f3e56b3e93bd6605ae5484b
arthurgabriel73 <arthurgabriel73@gmail.com>
2024-10-15 17:52:27 -0300

    feat: adds vpc data

--- src/modules/autenticacao/http.ts
+++ src/modules/autenticacao/http.ts
@@ -1,18 +1,22 @@
 import type { Functions } from 'serverless/aws';
 
 export const autenticacaoFunctionsHttp: Functions = {
-    autenticar: {
-        layers: [{ Ref: 'DependenciesNodeModulesLambdaLayer' }],
-        handler: '${self:custom.modulesDir}/autenticacao/handler.handler',
-        timeout: 10,
-        events: [
-            {
-                http: {
-                    method: 'POST',
-                    path: '/autenticar',
-                    cors: true
-                }
-            }
-        ]
+  autenticar: {
+    layers: [{ Ref: 'DependenciesNodeModulesLambdaLayer' }],
+    handler: '${self:custom.modulesDir}/autenticacao/handler.handler',
+    timeout: 10,
+    events: [
+      {
+        http: {
+        method: 'POST',
+        path: '/autenticar',
+        cors: true
+        }
+      }
+    ],
+    vpc: {
+      securityGroupIds: [process.env.SECURITY_GROUP_ID as string],
+      subnetIds: [process.env.SUBNET_ID_1A as string, process.env.SUBNET_ID_1B as string]
     }
+  }
 }
